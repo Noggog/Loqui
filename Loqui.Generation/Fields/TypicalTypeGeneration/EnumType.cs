816da8358bf25a04310c09942566c6aa5509400a
Justin Swanson <justin.c.swanson@gmail.com>
2017-10-22 03:28:05 -0500

    EnumType no longer tries to parse namespace.  Was conflicting with mechanics when defined enum was inside a class

--- Loqui.Generation/Fields/TypicalTypeGeneration/EnumType.cs
+++ Loqui.Generation/Fields/TypicalTypeGeneration/EnumType.cs
@@ -7,7 +7,6 @@ namespace Loqui.Generation
     public class EnumType : PrimitiveType
     {
         public string EnumName;
-        public string NameSpace;
         public bool Nullable;
 
         public override string TypeName => $"{EnumName}{(Nullable ? "?" : string.Empty)}";
@@ -29,21 +28,6 @@ namespace Loqui.Generation
             EnumName = node.GetAttribute<string>(
                 "enumName",
                 throwException: true);
-
-            int periodIndex = EnumName.LastIndexOf('.');
-            if (periodIndex != -1)
-            {
-                NameSpace = EnumName.Substring(0, periodIndex);
-                EnumName = EnumName.Substring(periodIndex + 1);
-            }
-        }
-
-        public override IEnumerable<string> GetRequiredNamespaces()
-        {
-            if (!string.IsNullOrWhiteSpace(NameSpace))
-            {
-                yield return NameSpace;
-            }
         }
     }
 }
