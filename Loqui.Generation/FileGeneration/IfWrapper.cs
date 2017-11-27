a0b46b2a8f6ee8cf13d2e64410e937e2f2f7448f
Justin Swanson <justin.c.swanson@gmail.com>
2017-11-26 22:14:31 -0600

    IfWrapper

---
+++ Loqui.Generation/FileGeneration/IfWrapper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loqui.Generation
+{
+    public class IfWrapper : IDisposable
+    {
+        FileGeneration fg;
+        bool first;
+        public List<string> Checks = new List<string>();
+        bool ands;
+
+        public IfWrapper(FileGeneration fg, bool ANDs, bool first = true)
+        {
+            this.ands = ANDs;
+            this.first = first;
+            this.fg = fg;
+        }
+
+        public void Dispose()
+        {
+            if (Checks.Count == 0) throw new ArgumentException();
+            using (var line = new LineWrapper(fg))
+            {
+                if (!first)
+                {
+                    fg.Append("else ");
+                }
+                fg.Append("if (");
+                fg.Append(Checks[0]);
+                if (Checks.Count == 1)
+                {
+                    fg.Append(")");
+                    return;
+                }
+            }
+            using (new DepthWrapper(fg))
+            {
+                for (int i = 1; i < Checks.Count; i++)
+                {
+                    using (new LineWrapper(fg))
+                    {
+                        if (this.ands)
+                        {
+                            fg.Append("&& ");
+                        }
+                        else
+                        {
+                            fg.Append("|| ");
+                        }
+                        fg.Append(Checks[i]);
+                        if (i == Checks.Count - 1)
+                        {
+                            fg.Append(")");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
