dad823de599e65afb7bb82f1951951c1e1762aa9
Justin Swanson <justin.c.swanson@gmail.com>
2018-11-24 00:05:43 -0600

    PercentType equals generation fix

--- Loqui.Generation/Fields/TypicalTypeGeneration/PercentType.cs
+++ Loqui.Generation/Fields/TypicalTypeGeneration/PercentType.cs
@@ -6,5 +6,10 @@ namespace Loqui.Generation
     public class PercentType : DoubleType
     {
         public override Type Type => typeof(Percent);
+
+        public override void GenerateForEquals(FileGeneration fg, Accessor accessor, Accessor rhsAccessor)
+        {
+            fg.AppendLine($"if (!{accessor.DirectAccess}.Equals({rhsAccessor.DirectAccess})) return false;");
+        }
     }
 }
