6769a4677f137fc74e7cb2575c8a621a4e728924
Justin Swanson <justin.c.swanson@gmail.com>
2019-08-30 08:20:50 -0500

    P3Int16 support

---
+++ Loqui/Translators/Xml/P3Int16XmlTranslation.cs
@@ -0,0 +1,27 @@
+﻿using Loqui.Internal;
+using Noggog;
+using System;
+
+namespace Loqui.Xml
+{
+    public class P3Int16XmlTranslation : PrimitiveXmlTranslation<P3Int16>
+    {
+        public readonly static P3Int16XmlTranslation Instance = new P3Int16XmlTranslation();
+
+        protected override string GetItemStr(P3Int16 item)
+        {
+            return $"{item.X}, {item.Y}, {item.Z}";
+        }
+
+        protected override bool ParseNonNullString(string str, out P3Int16 value, ErrorMaskBuilder errorMask)
+        {
+            if (P3Int16.TryParse(str, out value))
+            {
+                return true;
+            }
+            errorMask.ReportExceptionOrThrow(
+                new ArgumentException($"Could not convert to {NullableName}"));
+            return false;
+        }
+    }
+}
