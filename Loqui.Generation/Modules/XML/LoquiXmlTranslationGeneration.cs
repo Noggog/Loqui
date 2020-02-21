e9e2e689a4879166023ae40705519facfe51f019
Justin Swanson <justin.c.swanson@gmail.com>
2020-02-20 20:33:30 -0600

    LoquiXml TryGet usage

--- Loqui.Generation/Modules/XML/LoquiXmlTranslationGeneration.cs
+++ Loqui.Generation/Modules/XML/LoquiXmlTranslationGeneration.cs
@@ -30,37 +30,50 @@ namespace Loqui.Generation
         {
             using (new BraceWrapper(fg, doIt: !this.XmlMod.TranslationMaskParameter))
             {
-                // We want to cache retrievals, in case it's a wrapper being written
-                fg.AppendLine($"var {typeGen.Name}Item = {itemAccessor.DirectAccess};");
-                var loquiGen = typeGen as LoquiType;
-                string line;
-                if (loquiGen.TargetObjectGeneration != null)
+                if (typeGen.HasBeenSet)
                 {
-                    line = $"(({this.XmlMod.TranslationWriteClassName(loquiGen.TargetObjectGeneration)})(({nameof(IXmlItem)}){typeGen.Name}Item).{this.XmlMod.TranslationWriteItemMember})";
+                    fg.AppendLine($"if ({itemAccessor.DirectAccess}.TryGet(out var {typeGen.Name}Item))");
+                    itemAccessor = $"{typeGen.Name}Item";
                 }
                 else
                 {
-                    line = $"(({this.XmlMod.TranslationWriteInterface})(({nameof(IXmlItem)}){typeGen.Name}Item).{this.XmlMod.TranslationWriteItemMember})";
+                    // We want to cache retrievals, in case it's a wrapper being written
+                    fg.AppendLine($"var {typeGen.Name}Item = {itemAccessor.DirectAccess};");
+                    itemAccessor = $"{typeGen.Name}Item";
                 }
-                using (var args = new ArgsWrapper(fg, $"{line}.Write{loquiGen.GetGenericTypes(getter: true, additionalMasks: new MaskType[] { MaskType.Normal })}"))
+
+                using (new BraceWrapper(fg, doIt: typeGen.HasBeenSet))
                 {
-                    args.Add($"item: {typeGen.Name}Item");
-                    args.Add($"{XmlTranslationModule.XElementLine.GetParameterName(objGen)}: {writerAccessor}");
-                    args.Add($"name: {nameAccessor}");
-                    if (typeGen.HasIndex)
+                    var loquiGen = typeGen as LoquiType;
+                    string line;
+                    if (loquiGen.TargetObjectGeneration != null)
                     {
-                        args.Add($"fieldIndex: (int){typeGen.IndexEnumName}");
+                        line = $"(({this.XmlMod.TranslationWriteClassName(loquiGen.TargetObjectGeneration)})(({nameof(IXmlItem)}){typeGen.Name}Item).{this.XmlMod.TranslationWriteItemMember})";
                     }
-                    args.Add($"errorMask: {errorMaskAccessor}");
-                    if (this.XmlMod.TranslationMaskParameter)
+                    else
                     {
+                        line = $"(({this.XmlMod.TranslationWriteInterface})(({nameof(IXmlItem)}){typeGen.Name}Item).{this.XmlMod.TranslationWriteItemMember})";
+                    }
+                    using (var args = new ArgsWrapper(fg, $"{line}.Write{loquiGen.GetGenericTypes(getter: true, additionalMasks: new MaskType[] { MaskType.Normal })}"))
+                    {
+                        args.Add($"item: {typeGen.Name}Item");
+                        args.Add($"{XmlTranslationModule.XElementLine.GetParameterName(objGen)}: {writerAccessor}");
+                        args.Add($"name: {nameAccessor}");
                         if (typeGen.HasIndex)
                         {
-                            args.Add($"translationMask: {translationMaskAccessor}?.GetSubCrystal({typeGen.IndexEnumInt})");
+                            args.Add($"fieldIndex: (int){typeGen.IndexEnumName}");
                         }
-                        else
+                        args.Add($"errorMask: {errorMaskAccessor}");
+                        if (this.XmlMod.TranslationMaskParameter)
                         {
-                            args.Add($"translationMask: {translationMaskAccessor}");
+                            if (typeGen.HasIndex)
+                            {
+                                args.Add($"translationMask: {translationMaskAccessor}?.GetSubCrystal({typeGen.IndexEnumInt})");
+                            }
+                            else
+                            {
+                                args.Add($"translationMask: {translationMaskAccessor}");
+                            }
                         }
                     }
                 }
