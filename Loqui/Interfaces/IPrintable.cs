11fff61cd4994403e5b1686ec5138e3b9aa5479a
Justin Swanson <justin.c.swanson@gmail.com>
2020-04-04 17:39:59 -0500

    IPrintable name made nullable

--- Loqui/Interfaces/IPrintable.cs
+++ Loqui/Interfaces/IPrintable.cs
@@ -8,6 +8,6 @@ namespace Loqui
 {
     public interface IPrintable
     {
-        void ToString(FileGeneration fg, string? name);
+        void ToString(FileGeneration fg, string? name = null);
     }
 }
