c67c2ab21c2970ac586bf6dea1984ea598321fb2
Justin Swanson <justin.c.swanson@gmail.com>
2021-01-18 14:01:37 -0600

    NET5_0 preprocessor fix

--- Loqui.Generation/ObjectGeneration.cs
+++ Loqui.Generation/ObjectGeneration.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
-#if NET_5
-using TaskCompletionSource = TaskCompletionSource;
+#if NET5_0
+using TaskCompletionSource = System.Threading.Tasks.TaskCompletionSource;
 #else
 using TaskCompletionSource = Noggog.TaskCompletionSource;
 #endif
