466f163698411b62187d0e5eaad5194e73bd7d97
Justin Swanson <justin.c.swanson@gmail.com>
2021-01-31 00:56:50 -0600

    Interfaces order alphabetically

--- Loqui.Generation/FileGeneration/ClassWrapper.cs
+++ Loqui.Generation/FileGeneration/ClassWrapper.cs
@@ -36,7 +36,7 @@ namespace Loqui.Generation
         public void Dispose()
         {
             var classLine = $"{EnumExt.ToStringFast_Enum_Only<PermissionLevel>(Public)} {(this.Static ? "static " : null)}{(this.New ? "new " : null)}{(this.Abstract ? "abstract " : null)}{(this.Partial ? "partial " : null)}{EnumExt.ToStringFast_Enum_Only<ObjectType>(Type)} {this.Name}";
-            var toAdd = this.Interfaces.ToList();
+            var toAdd = this.Interfaces.OrderBy(x => x).ToList();
             if (!string.IsNullOrWhiteSpace(this.BaseClass))
             {
                 toAdd.Insert(0, this.BaseClass);
