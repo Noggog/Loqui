48095b62e6bd31dbd7933ef07f007b15e1d5e264
Justin Swanson <justin.c.swanson@gmail.com>
2022-04-08 16:54:12 -0500

    IMapsToGetter

---
+++ Loqui/Interfaces/IMapsToGetter.cs
@@ -0,0 +1,8 @@
+namespace Loqui.Interfaces;
+
+/// <summary>
+/// Helper interface that helps map an interface to the getter version of an interface
+/// </summary>
+public interface IMapsToGetter<IGetter>
+{
+}
